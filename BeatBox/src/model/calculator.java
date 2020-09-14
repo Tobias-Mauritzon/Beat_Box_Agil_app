df36ba0e99184f1ca2be60fe6c5c504ab1677275
Tobias-Mauritzon <tobmau@student.chalmers.se>
2020-09-14 11:08:23 +0200

    generator prototyp

--- BeatBox/src/model/calculator.java
+++ BeatBox/src/model/calculator.java
@@ -1,11 +1,92 @@
 package model;
 
+import java.util.Random;
+
 public class calculator {
 	int num1;
 	int num2;
 	
 	public static void main(String[] args) 
 	{
-		System.out.print("Hello World!");
+		int[] stor = {1,100};
+		int[] mod = {1,2,3,4};
+		String[] s = rec(5, stor,new String[2], mod);
+		
+		System.out.println(s[0]);
+		System.out.println(s[1]);
+	}
+	
+	private static String[] rec(int antalTal, int[] storlek, String[] talOchSvar, int[] mod) {
+		
+		if(antalTal != 0) {
+			antalTal--;
+			if(talOchSvar[1] != null) {
+				Random rand = new Random();
+				
+				int genNumber = rand.nextInt((storlek[1] - (storlek[0]) + 1) + (storlek[0]));
+				boolean allowed = false;
+				int whichMod = 0;
+				
+				while(!allowed) {
+					whichMod = (int) ((Math.random() * (4 - 1))+1);
+					
+					for(int i : mod) {
+						if(i == whichMod) {
+							allowed = true;
+						}
+					}
+				}
+				
+				
+				if(whichMod == 1) {
+					
+					talOchSvar[0] = talOchSvar[0]+" + "+genNumber;
+					int nyttSvar = Integer.parseInt(talOchSvar[1]) + genNumber;
+					talOchSvar[1] = Integer.toString(nyttSvar);
+					
+				}else if(whichMod == 2){
+					
+					talOchSvar[0] = talOchSvar[0]+" - "+genNumber;
+					int nyttSvar = Integer.parseInt(talOchSvar[1]) - genNumber;
+					talOchSvar[1] = Integer.toString(nyttSvar);
+					
+				}else if(whichMod == 3){
+					boolean run = true;
+					while(run) {
+						
+						try {
+							talOchSvar[0] = "("+talOchSvar[0]+")/"+genNumber;
+							int nyttSvar = Integer.parseInt(talOchSvar[1]) / genNumber;
+							talOchSvar[1] = Integer.toString(nyttSvar);
+							run= false;
+						}catch(Exception e) {
+							genNumber = rand.nextInt((storlek[1] - (storlek[0]) + 1) + (storlek[0]));
+						}
+					}
+															
+				}else if(whichMod == 4){
+					
+					talOchSvar[0] = "("+talOchSvar[0]+") * "+genNumber;
+					int nyttSvar = Integer.parseInt(talOchSvar[1]) * genNumber;
+					talOchSvar[1] = Integer.toString(nyttSvar);
+					
+				}
+				
+				
+				
+			}else {
+				Random rand = new Random();
+				
+				int genNumber = rand.nextInt((storlek[1] - (storlek[0])+1)+(storlek[0]));
+				
+				talOchSvar[0] = Integer.toString(genNumber);
+				talOchSvar[1] = Integer.toString(genNumber);
+			}
+			
+			talOchSvar = rec(antalTal, storlek, talOchSvar, mod);
+		}
+		
+		
+		return talOchSvar;
 	}
 }
