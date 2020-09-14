585e6f86171fb621b1ae931a76a49f5c050580bc
Greppe <Greppe@student.chalmers.se>
2020-09-14 13:52:48 +0200

    Added EventHandler
    Added EventHandler so button press does something

--- BeatBox/src/view/ProblemGUI.java
+++ BeatBox/src/view/ProblemGUI.java
@@ -4,17 +4,23 @@ import javafx.scene.control.Button;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class ProblemGUI extends Application{
+public class ProblemGUI extends Application implements EventHandler<ActionEvent>{
+	
+	Button button;
+	
     @Override
     public void start(Stage primaryStage) {
         try {
         	primaryStage.setTitle("MathGame");
-        	Button button = new Button();
-        	button.setText("ClickMe");
+        	button = new Button();
+        	button.setText("Answer");
+        	button.setOnAction(this);
         	StackPane layout = new StackPane();
         	layout.getChildren().add(button);
             Scene scene = new Scene(layout,400,400);
@@ -25,8 +31,18 @@ public class ProblemGUI extends Application{
             e.printStackTrace();
         }
     }
+    
 
     public static void main(String[] args) {
         launch(args);
     }
+
+	@Override
+	public void handle(ActionEvent event) {
+		if(event.getSource()== button) 
+		{
+			System.out.println("You Answered");
+		}
+		
+	}
 }
