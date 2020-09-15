d8109506c77f453742e7dfb5c76cf564f20ced33
Philip <60391080+phiaxen@users.noreply.github.com>
2020-09-15 14:47:17 +0200

    Implement response with fade effect
    La till correct/wrong respons

--- BeatBox/src/controller/SampleController.java
+++ BeatBox/src/controller/SampleController.java
@@ -3,6 +3,7 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,53 +11,92 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 import model.testGenerator;
 
 public class SampleController implements Initializable{
-		@FXML
-	    private Text problemText;
+	
+	@FXML
+    private VBox responseBox;
+	
+	@FXML
+    private Text problemText;
 
-	    @FXML
-	    private TextField answerText;
+    @FXML
+    private TextField answerText;
 
-	    @FXML
-	    private Button button;
-	    
-	    
-	    private testGenerator testGen;
+    @FXML
+    private Button button;
+    
+    @FXML
+    private Text responseText;
+    
+    private testGenerator testGen;
 
-	    private String text;
-	    private String answer;
-	    
-	    @FXML
-	    void AnswerText(ActionEvent event) {
-	    	answer();
-	    }
-	    
-	   @FXML
-	    void ButtonClicked(ActionEvent event) {	   
-		   answer();
-	    }
-	 
-	   private void answer() {
-		   if(answerText.getText().equals(answer)) {
-			   System.out.println("CORRECT");
-			   String[] a = testGen.getNextProblem();
-			   text = a[0];
-			   answer = a[1];
-			   problemText.setText(text);
-			   answerText.clear();
-			   answerText.requestFocus();
-		   }else {
-			   System.out.println("WRONG");
-			   answerText.clear();
-			   answerText.requestFocus();
-		   }
-		   
-		   System.out.println("Clicked button");
+    private String text;
+    private String answer;
+    
+    
+    @FXML
+    void AnswerText(ActionEvent event) {
+    	answer();
+    }
+    
+   @FXML
+    void ButtonClicked(ActionEvent event) {	   
+	   answer();
+    }
+ 
+   private void answer() {
+	   if(answerText.getText().equals(answer)) {
+		   System.out.println("CORRECT");
+		   String[] a = testGen.getNextProblem();
+		   text = a[0];
+		   answer = a[1];
+		   problemText.setText(text);
+		   answerText.clear();
+		   answerText.requestFocus();
+		   showResponse(true);
+	   }else {
+		   System.out.println("WRONG");
+		   answerText.clear();
+		   answerText.requestFocus();
+		   showResponse(false);
 	   }
+	   
+	   System.out.println("Clicked button");
+   }
 
+   
+   private void showResponse(boolean correct) {
+	   if(correct) {
+		   responseText.setText("CORRECT!");
+		   responseText.setFill(Color.LIMEGREEN);
+		   responseBox.setVisible(true); 
+		   
+		   FadeTransition ft = new FadeTransition(Duration.millis(1500), responseBox);
+		   ft.setFromValue(1);
+		   ft.setToValue(0);
+		   ft.play();
+		  
+	   }else {
+		   responseText.setText("WRONG!");
+		   responseText.setFill(Color.RED);
+		   responseBox.setVisible(true);
+		   
+		   FadeTransition ft = new FadeTransition(Duration.millis(1500), responseBox);
+		   ft.setFromValue(1);
+		   ft.setToValue(0);
+		   ft.play();
+		   
+		   
+	   }
+   }
+	   
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
@@ -67,6 +107,5 @@ public class SampleController implements Initializable{
 		answerText.clear();
 		problemText.setText(text);
 		problemText.setFocusTraversable(false);
-		
 	}
 }
