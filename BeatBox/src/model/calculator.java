1ad68adcf08e6a9ccd58beb228cb2cac9473a797
Tobias-Mauritzon <tobmau@student.chalmers.se>
2020-09-15 11:16:56 +0200

    uppdated logic, new names, more comments

--- BeatBox/src/model/calculator.java
+++ BeatBox/src/model/calculator.java
@@ -9,80 +9,77 @@ public class calculator {
 	public static void main(String[] args) 
 	{
 		//number size
-		int[] stor = {1,100};
+		int[] size = {1,100};
 		// allowed modifiers
 		int[] mod = {1,2,3,4};
-		String[] s = rec(5, stor,new String[2], mod);
+		String[] s = generator(5, size,new String[2], mod);
 		
 		System.out.println(s[0]);
 		System.out.println(s[1]);
 	}
 	
-	
 	/**
-	 * @param antalTal
-	 * @param storlek
-	 * @param talOchSvar
-	 * @param mod
-	 * @return
+	 * @param numbers the number of numbers in the expression
+	 * @param numberSize allowed number size {small, big}
+	 * @param expressionAndAwnser new String[2]
+	 * @param modifiers if list contains 1 = addition, 2 = subtraction, 3 = division, 4 = multiplication. list can be any size
+	 * @return string array. array[0] = expression, array[1] = awnser
 	 */
-	private static String[] rec(int antalTal, int[] storlek, String[] talOchSvar, int[] mod) {
-		
-		if(antalTal != 0) {
-			antalTal--;
-			if(talOchSvar[1] != null) {
-				Random rand = new Random();
+	private static String[] generator(int numbers, int[] numberSize, String[] expressionAndAwnser, int[] modifiers) {
+		Random rand = new Random();
+		if(numbers > 0) {
+			numbers--;
+			
+			// generates the next number for the expression
+			int genNumber = rand.nextInt((numberSize[1] - (numberSize[0]))+1) + (numberSize[0]);
+			double newAwnser;
+			if(expressionAndAwnser[1] != null) {				
 				
-				// generates the next number for the expression
-				int genNumber = rand.nextInt((storlek[1] - (storlek[0]) + 1) + (storlek[0]));
 				boolean allowed = false;
 				int whichMod = 0;
 				
 				while(!allowed) {
-					// generates a number 1 - 4 radomly for modifier
-					whichMod = (int) ((Math.random() * (4 - 1))+1);
-					
-					for(int i : mod) {
-						if(i == whichMod) {
+					// generates a number 1 - 4 randomly for modifier
+					whichMod = rand.nextInt(4)+1;					
+					for(int m : modifiers) {
+						if(m == whichMod) {
 							allowed = true;
 						}
 					}
-				}				
+				}
+				
 				if(whichMod == 1) {					
-					talOchSvar[0] = talOchSvar[0]+" + "+genNumber;
-					int nyttSvar = Integer.parseInt(talOchSvar[1]) + genNumber;
-					talOchSvar[1] = Integer.toString(nyttSvar);					
+					expressionAndAwnser[0] = expressionAndAwnser[0]+" + "+genNumber;
+					newAwnser = Double.parseDouble(expressionAndAwnser[1]) + genNumber;
+					expressionAndAwnser[1] = Double.toString(newAwnser);					
 				}else if(whichMod == 2){					
-					talOchSvar[0] = talOchSvar[0]+" - "+genNumber;
-					int nyttSvar = Integer.parseInt(talOchSvar[1]) - genNumber;
-					talOchSvar[1] = Integer.toString(nyttSvar);					
+					expressionAndAwnser[0] = expressionAndAwnser[0]+" - "+genNumber;
+					newAwnser = Double.parseDouble(expressionAndAwnser[1]) - genNumber;
+					expressionAndAwnser[1] = Double.toString(newAwnser);					
 				}else if(whichMod == 3){
 					boolean run = true;
 					while(run) {						
 						try {
-							talOchSvar[0] = "("+talOchSvar[0]+")/"+genNumber;
-							int nyttSvar = Integer.parseInt(talOchSvar[1]) / genNumber;
-							talOchSvar[1] = Integer.toString(nyttSvar);
-							run= false;
+							expressionAndAwnser[0] = "("+expressionAndAwnser[0]+")/"+genNumber;
+							newAwnser = Double.parseDouble(expressionAndAwnser[1]) / genNumber;
+							expressionAndAwnser[1] = Double.toString(newAwnser);
+							run = false;
 						}catch(Exception e) {
-							genNumber = rand.nextInt((storlek[1] - (storlek[0]) + 1) + (storlek[0]));
+							genNumber = rand.nextInt((numberSize[1] - (numberSize[0]))) + (numberSize[0]);
 						}
 					}															
-				}else if(whichMod == 4){					
-					talOchSvar[0] = "("+talOchSvar[0]+") * "+genNumber;
-					int nyttSvar = Integer.parseInt(talOchSvar[1]) * genNumber;
-					talOchSvar[1] = Integer.toString(nyttSvar);					
+				}else if(whichMod == 4){
+					// the parentheses are a feature
+					expressionAndAwnser[0] = "("+expressionAndAwnser[0]+") * "+genNumber;
+					newAwnser = Double.parseDouble(expressionAndAwnser[1]) * genNumber;
+					expressionAndAwnser[1] = Double.toString(newAwnser);					
 				}								
-			}else {
-				Random rand = new Random();
-				
-				int genNumber = rand.nextInt((storlek[1] - (storlek[0])+1)+(storlek[0]));
-				
-				talOchSvar[0] = Integer.toString(genNumber);
-				talOchSvar[1] = Integer.toString(genNumber);
+			}else {								
+				expressionAndAwnser[0] = Integer.toString(genNumber);
+				expressionAndAwnser[1] = Integer.toString(genNumber);
 			}			
-			talOchSvar = rec(antalTal, storlek, talOchSvar, mod);
+			expressionAndAwnser = generator(numbers, numberSize, expressionAndAwnser, modifiers);
 		}		
-		return talOchSvar;
+		return expressionAndAwnser;
 	}
 }
