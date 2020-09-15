b3bec4ea85260ea86669ff99caa41b0aa9df9398
Tobias-Mauritzon <tobmau@student.chalmers.se>
2020-09-15 13:10:08 +0200

    variable name

--- BeatBox/src/model/calculator.java
+++ BeatBox/src/model/calculator.java
@@ -58,20 +58,20 @@ public class calculator {
 	 * 
 	 * @param numbers the number of numbers in the expression
 	 * @param numberSize allowed number size {small, big}
-	 * @param expressionAndAwnser new String[2]
+	 * @param expressionAndAnswer new String[2]
 	 * @param modifiers if array contains 1 = addition, 2 = subtraction, 3 = division, 4 = multiplication, 
 	 * 			5 = power of 2. array can be any size
 	 * @return String array. array[0] = expression, array[1] = answer
 	 */
-	public static String[] generate(int numbers, int[] numberSize, String[] expressionAndAwnser, int[] modifiers) {
+	public static String[] generate(int numbers, int[] numberSize, String[] expressionAndAnswer, int[] modifiers) {
 		Random rand = new Random();
 		if(numbers > 0) {
 			numbers--;
 			
 			// generates the next number for the expression
 			int genNumber = rand.nextInt((numberSize[1] - (numberSize[0]))+1) + (numberSize[0]);
-			double newAwnser;
-			if(expressionAndAwnser[1] != null) {				
+			double newAnswer;
+			if(expressionAndAnswer[1] != null) {				
 				
 				boolean allowed = false;
 				int whichMod = 0;
@@ -87,20 +87,20 @@ public class calculator {
 				}
 				
 				if(whichMod == 1) {					
-					expressionAndAwnser[0] = expressionAndAwnser[0]+" + "+genNumber;
-					newAwnser = Double.parseDouble(expressionAndAwnser[1]) + genNumber;
-					expressionAndAwnser[1] = Double.toString(newAwnser);					
+					expressionAndAnswer[0] = expressionAndAnswer[0]+" + "+genNumber;
+					newAnswer = Double.parseDouble(expressionAndAnswer[1]) + genNumber;
+					expressionAndAnswer[1] = Double.toString(newAnswer);					
 				}else if(whichMod == 2){					
-					expressionAndAwnser[0] = expressionAndAwnser[0]+" - "+genNumber;
-					newAwnser = Double.parseDouble(expressionAndAwnser[1]) - genNumber;
-					expressionAndAwnser[1] = Double.toString(newAwnser);					
+					expressionAndAnswer[0] = expressionAndAnswer[0]+" - "+genNumber;
+					newAnswer = Double.parseDouble(expressionAndAnswer[1]) - genNumber;
+					expressionAndAnswer[1] = Double.toString(newAnswer);					
 				}else if(whichMod == 3){
 					boolean run = true;
 					while(run) {						
 						try {
-							expressionAndAwnser[0] = "("+expressionAndAwnser[0]+")/"+genNumber;
-							newAwnser = Double.parseDouble(expressionAndAwnser[1]) / genNumber;
-							expressionAndAwnser[1] = Double.toString(newAwnser);
+							expressionAndAnswer[0] = "("+expressionAndAnswer[0]+")/"+genNumber;
+							newAnswer = Double.parseDouble(expressionAndAnswer[1]) / genNumber;
+							expressionAndAnswer[1] = Double.toString(newAnswer);
 							run = false;
 						}catch(Exception e) {
 							genNumber = rand.nextInt((numberSize[1] - (numberSize[0]))) + (numberSize[0]);
@@ -108,21 +108,21 @@ public class calculator {
 					}															
 				}else if(whichMod == 4){
 					// the parentheses are a feature
-					expressionAndAwnser[0] = "("+expressionAndAwnser[0]+") * "+genNumber;
-					newAwnser = Double.parseDouble(expressionAndAwnser[1]) * genNumber;
-					expressionAndAwnser[1] = Double.toString(newAwnser);					
+					expressionAndAnswer[0] = "("+expressionAndAnswer[0]+") * "+genNumber;
+					newAnswer = Double.parseDouble(expressionAndAnswer[1]) * genNumber;
+					expressionAndAnswer[1] = Double.toString(newAnswer);					
 				}else if(whichMod == 5){
 					// the parentheses are a feature
-					expressionAndAwnser[0] = "("+expressionAndAwnser[0]+") ^ "+2;
-					newAwnser = Math.pow(Double.parseDouble(expressionAndAwnser[1]), 2);
-					expressionAndAwnser[1] = Double.toString(newAwnser);					
+					expressionAndAnswer[0] = "("+expressionAndAnswer[0]+") ^ "+2;
+					newAnswer = Math.pow(Double.parseDouble(expressionAndAnswer[1]), 2);
+					expressionAndAnswer[1] = Double.toString(newAnswer);					
 				}
 			}else {								
-				expressionAndAwnser[0] = Integer.toString(genNumber);
-				expressionAndAwnser[1] = Integer.toString(genNumber);
+				expressionAndAnswer[0] = Integer.toString(genNumber);
+				expressionAndAnswer[1] = Integer.toString(genNumber);
 			}			
-			expressionAndAwnser = generate(numbers, numberSize, expressionAndAwnser, modifiers);
+			expressionAndAnswer = generate(numbers, numberSize, expressionAndAnswer, modifiers);
 		}		
-		return expressionAndAwnser;
+		return expressionAndAnswer;
 	}
 }
