a5d709a513c26c2731d6e5eae3fddb6e92444711
joantfolk <antfolk@student.chalmers.se>
2020-09-17 14:04:24 +0200

    Pushat med tester

--- BeatBox/src/model/calculator.java
+++ BeatBox/src/model/calculator.java
@@ -4,65 +4,82 @@ import java.util.LinkedList;
 import java.util.Random;
 
 /**
- * @author Tobias & Joachim
  * Class used to generate random expression depending on different modifiers
+ * @author Tobias & Joachim
+ * @version 1.0
+ * @since 2020-09-17
  */
 
 enum Operator{
-	ADD, SUB, DIV, MUL, POW;
+	ADD, SUB, DIV, MUL, EXP;
 }
 
 public class calculator {
 		
 	private LinkedList<String> uniqueness; 
+	
 	public calculator() {
 		this.uniqueness = new LinkedList<String>();
 	}
 	
 	/**
-	 * guarantees the generated number has not appeared in the last 10 numbers
-	 * 
+	 * Used to generate unique problems if there are more than ten possible outcomes with current parameters.
+	 * 		Otherwise ignore uniqueness.
 	 * @param numbers the number of numbers in the expression
 	 * @param numberSize allowed number size {small, big}
-	 * @param modifiers if array contains 1 = addition, 2 = subtraction, 3 = division, 4 = multiplication, 
-	 * 			5 = power of 2. array can be any size
+	 * @param modifiers array of enum Operators that can be used to generate expressions 
 	 * @return String array. array[0] = expression, array[1] = awnser
 	 */
-	public String[] uniqueGeneration(int numbers, int[] numberSize, Operator[] modifiers) {
+	public String[] uniqueGeneration(int numbers, int[] numberRange, Operator[] modifiers) {
 		boolean unique = false;
 		String[] returnString = new String[2];
-	
-		while(!unique) {
-			if(uniqueness.isEmpty()) unique = true;
-			returnString = generate(numbers, numberSize, new String[2], modifiers);
-			for(String s : uniqueness) {
-				if(s.equals(returnString[0])) {
-					unique = false;
-					break;
+		try {
+			if(10 < (modifiers.length * Math.pow((numberRange[1] - numberRange[0] + 1), numbers))) {
+				while(!unique) {
+					if(uniqueness.isEmpty()) unique = true;
+					returnString = generate(numbers, numberRange, modifiers);
+					for(String s : uniqueness) {
+						if(s.equals(returnString[0])) {
+							unique = false;
+							break;
+						}
+						unique = true;
+					}
 				}
-				unique = true;
+				
+				if(uniqueness.size() >= 10) {
+					uniqueness.removeFirst();
+				}
+				uniqueness.addLast(returnString[0]);
+				
+				return returnString;
+				
+			}else {
+				return generate(numbers, numberRange, modifiers);
 			}
-		}
-		
-		if(uniqueness.size() >= 10) {
-			uniqueness.removeFirst();
-		}
-		uniqueness.addLast(returnString[0]);
-		
-		return returnString;
+		}catch(IllegalArgumentException e) {
+			throw e;
+		}	
 	}
 	
-	
+	/**
+	 * testing
+	 * @param args
+	 */
 	public static void main(String[] args) 
 	{
 		//number size
-		int[] size = {1,10};
+		int[] size = {0,100};
 		// allowed modifiers
-		Operator[] mod = {Operator.ADD, Operator.SUB};
-		String[] s = generate(3, size,new String[2], mod);
-		
-		System.out.println(s[0]);
-		System.out.println(s[1]);
+		Operator[] mod = {Operator.ADD};
+		String[] s = new String[2];
+		try {
+			s = generate(3, size, mod);
+			System.out.println(s[0]);
+			System.out.println(s[1]);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}	
 	}
 	
 	/**
@@ -74,8 +91,16 @@ public class calculator {
 	 * @param modifiers array of enum Operators that can be used to generate expressions 
 	 * @return String array. array[0] = expression, array[1] = answer
 	 */
-	public static String[] generate(int numbers, int[] numberSize, String[] expressionAndAnswer, Operator[] modifiers) {
+	public static String[] generate(int numbers, int[] numberSize, Operator[] modifiers) {
 		Random rand = new Random();
+		
+		if(numbers < 2) {
+			throw new IllegalArgumentException("Number of terms must be grater than 2!");
+		}
+		
+		if(numberSize[0] > numberSize[1]) {
+			throw new IllegalArgumentException("The real interval has to in the format [smaller, bigger]!");
+		}
 
 		return gen(numbers, numberSize, modifiers, rand);
 	}
@@ -111,19 +136,31 @@ public class calculator {
 				returnVal[1] = Double.toString(Double.parseDouble(rightRet[1]) - Double.parseDouble(leftRet[1]));
 				break;
 			case DIV:
-				returnVal[0] = "(" + rightRet[0] + " / " + leftRet[0] + ")";
-				returnVal[1] = Double.toString(Double.parseDouble(rightRet[1]) / Double.parseDouble(leftRet[1]));
+				boolean div = true;
+				while(div) {						
+					if(Double.parseDouble(leftRet[1]) != 0.0) {//Division by zero 
+						returnVal[0] = "(" + rightRet[0] + " / " + leftRet[0] + ")";
+						returnVal[1] = Double.toString(Double.parseDouble(rightRet[1]) / Double.parseDouble(leftRet[1]));
+						div = false;
+					}else {
+						leftRet = gen(left, numberRange, modifiers, rand); 
+					}
+				}	
 				break;
 			case MUL:
 				returnVal[0] = "(" + rightRet[0] + " * " + leftRet[0] + ")";
 				returnVal[1] = Double.toString(Double.parseDouble(rightRet[1]) * Double.parseDouble(leftRet[1]));
 				break;
-			case POW:
+			case EXP:
 				returnVal[0] = "(" + rightRet[0] + " ^ " + leftRet[0] + ")";
 				returnVal[1] = Double.toString(Math.pow(Double.parseDouble(rightRet[1]), Double.parseDouble(leftRet[1])));
 				break;
 			}
 		}				
 		return returnVal;
+	}
+	
+	public LinkedList<String> getList(){
+		return uniqueness;
 	}
 }
