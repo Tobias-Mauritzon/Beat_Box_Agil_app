cb0bb7b1f3db255aae5326d4476fe0de6299fad2
tehAndrew <andpa_kontakt@live.se>
2020-09-18 15:58:02 +0200

    Integrated fxml

--- BeatBox/src/model/Main.java
+++ BeatBox/src/model/Main.java
@@ -1,34 +1,31 @@
 package model;
 
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
  
 public class Main extends Application {
-    public static void main(String[] args) {
+    private VBox root;
+	
+	public static void main(String[] args) {
         launch(args);
     }
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+    	try {
+    		root = (VBox) FXMLLoader.load(getClass().getResource("/view/MenuGUI.fxml"));
+    	} catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	Scene scene = new Scene(root, root.getWidth(), root.getHeight());
+    	primaryStage.setScene(scene);
+    	primaryStage.show();
     }
 }
