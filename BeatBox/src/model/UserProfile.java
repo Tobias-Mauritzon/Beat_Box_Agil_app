5888643c86fad7e4e803f8dc6e4b6826a2ee41e6
Tobias-Mauritzon <tobmau@student.chalmers.se>
2020-09-22 11:38:26 +0200

    save load method skeleton

--- BeatBox/src/model/UserProfile.java
+++ BeatBox/src/model/UserProfile.java
@@ -1,20 +1,78 @@
 package model;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class UserProfile {
+	
+	// user name
+	private String name;
+	
+	// all problems stored as they come in
+	private LinkedList<Problem> history;
+	
+	//mapybe <recordType(as String), Problem>
+	private HashMap<String,Integer> records;
 
+	public UserProfile(String name) {
+		this.name = name;
+		this.history = new LinkedList<Problem>();
+		this.records = new HashMap<String,Integer>();
+	}
+	
+	public void loadProfile(String name) {
+		/* TODO
+		 * method needs proper implementation
+		 * should load from a sereilised save if possible
+		*/
+		this.history = new LinkedList<Problem>();
+		this.records = new HashMap<String,Integer>();
+	}
+	
+	public void saveProfile() {
+		
+	}
+	
+	/*
+	+ saveProfile() : void
+	+ loadProfile(name : String) : UserProfile
+	+ getName() : String
+	+ getHistory() : LinkedList<String>
+	+ getRecords() : HashMap<String, int> 
+	+ setName(name : String) : void
+	+ setHistory(history : LinkedList<String>) : void
+	+ setRecords(record : HashMap<String, int>) : void
+
+	*/
+	
+	/**Private problem class created to enable easy sorting 
+	 * and categorization
+	 * 
+	 * @author Tobias
+	 *
+	 */
 	private class Problem{
 		private final String problem;
-		private final String awnser;
+		private final String answer;
+		private final String correctAnswer;
 		private final Date date;
 		private final int points;
 		private final int timeRequierd;
 		private final Operator[] modifiers;
 		
-		public Problem(String problem, String awnser, int points, int timeRequierd, Operator[] modifiers) {
+		/**Initialises the Problem class with requierd values
+		 * 
+		 * @param problem as a Stirng
+		 * @param awnser to the problem as a String
+		 * @param points for the solve or failiure
+		 * @param timeRequierd to solve the problem
+		 * @param modifiers used to create the problem
+		 */
+		public Problem(String problem, String answer, String correctAnswer, int points, int timeRequierd, Operator[] modifiers) {
 			this.problem = problem;
-			this.awnser = awnser;
+			this.answer = answer;
+			this.correctAnswer = correctAnswer;
 			this.date =  new Date();
 			this.points = points;
 			this.timeRequierd = timeRequierd;
@@ -25,7 +83,10 @@ public class UserProfile {
 			return problem;
 		}
 		public String getAwnser() {
-			return awnser;
+			return answer;
+		}
+		public String getcCrrectAnswer() {
+			return correctAnswer;
 		}
 		public Date getDate() {
 			return date;
